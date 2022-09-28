290d63315fae4cbff1c61e7daff0f9963dc8c989
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-27 21:52:06 -0300

    .

--- src/app/componentes/form-afiliacion-dni/form-afiliacion-dni.component.ts
+++ src/app/componentes/form-afiliacion-dni/form-afiliacion-dni.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
+import { WebcamImage, WebcamUtil } from 'ngx-webcam';
 import { Observable, Subject } from 'rxjs';
 
 @Component({
@@ -10,43 +10,37 @@ import { Observable, Subject } from 'rxjs';
 export class FormAfiliacionDniComponent implements OnInit {
 
   public showWebcam = true;
-  public allowCameraSwitch = true;
+  public allowCameraSwitch = false;
   public multipleWebcamsAvailable = false;
   public deviceId: string;
   public videoOptions: MediaTrackConstraints = {
   };
 
-  public errors: WebcamInitError[] = [];
-
   public webcamImage: WebcamImage = null;
 
   private trigger: Subject<void> = new Subject<void>();
-  private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
 
   constructor() { }
 
   ngOnInit(): void {
     WebcamUtil.getAvailableVideoInputs()
       .then((mediaDevices: MediaDeviceInfo[]) => {
-        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length ===  1;
       });
   }
 
   public triggerSnapshot(): void {
-    this.trigger.next();
-    this.toggleWebcam();
+      if (!this.webcamImage) {
+        this.trigger.next();
+        this.toggleWebcam();
+      } else {
+        this.webcamImage = null;
+        this.toggleWebcam();
+      }
   }
 
   public toggleWebcam(): void {
     this.showWebcam = !this.showWebcam;
-  }
-
-  public handleInitError(error: WebcamInitError): void {
-    this.errors.push(error);
-  }
-
-  public showNextWebcam(directionOrDeviceId: boolean|string): void {
-    this.nextWebcam.next(directionOrDeviceId);
   }
 
   public handleImage(webcamImage: WebcamImage): void {
@@ -54,16 +48,7 @@ export class FormAfiliacionDniComponent implements OnInit {
     this.webcamImage = webcamImage;
   }
 
-  public cameraWasSwitched(deviceId: string): void {
-    console.log('active device: ' + deviceId);
-    this.deviceId = deviceId;
-  }
-
   public get triggerObservable(): Observable<void> {
     return this.trigger.asObservable();
-  }
-
-  public get nextWebcamObservable(): Observable<boolean|string> {
-    return this.nextWebcam.asObservable();
   }
 }
