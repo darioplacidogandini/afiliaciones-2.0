3d894320442d311730bff0867d9412a8db2f047c
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-10-25 20:53:28 -0300

    .

---
+++ src/app/servicios/afiliados.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Afiliado } from '../model/afiliado.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AfiliadosService {
+
+  constructor() { }
+}
